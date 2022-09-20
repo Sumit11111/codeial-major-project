958a80e2c3304add42fe8c61a036e2a375dff77b
Sumit11111 <sumitdhanda1111@gmail.com>
2022-09-20 16:51:54 +0530

    home route set with routes and controller

---
+++ routes/index.js
@@ -0,0 +1,10 @@
+const express=require('express');
+const router=express.Router();
+const home_controller=require('../controllers/home_controller');
+
+console.log('router loaded');
+
+//home route
+router.get('/',home_controller.home);
+
+module.exports=router;
